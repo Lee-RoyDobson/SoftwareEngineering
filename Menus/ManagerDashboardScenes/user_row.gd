b11bf461f9560a54c23e455e05679b23c9e57389
Niall <160640447+NiallBark@users.noreply.github.com>
2025-04-24 18:05:44 +0100

    Manager Dashboard

---
+++ Menus/ManagerDashboardScenes/user_row.gd
@@ -0,0 +1,19 @@
+extends HBoxContainer
+# Define a custom signal which sends the username and the assigned task name.
+
+signal task_assigned(username: String, task_name: String)
+
+func _on_memory_button_pressed() -> void:
+	var username = $UserName.text
+	print("Assigning Task 1 to user: " + username)
+	emit_signal("task_assigned", username, "Task 1")
+	
+func _on_focus_button_pressed() -> void:
+	var username = $UserName.text
+	print("Assigning Task 2 to user: " + username)
+	emit_signal("task_assigned", username, "Task 2")
+	
+func _on_reaction_button_pressed() -> void:
+	var username = $UserName.text
+	print("Assigning Task 3 to user: " + username)
+	emit_signal("task_assigned", username, "Task 3")
