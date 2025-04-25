bf07e80eeea0c2973644e3169991e51589aa599e
Niall <160640447+NiallBark@users.noreply.github.com>
2025-04-25 15:07:50 +0100

    fixed bugs brother

--- Menus/ManagerDashboardScenes/user_row.gd
+++ Menus/ManagerDashboardScenes/user_row.gd
@@ -1,22 +1,22 @@
 extends HBoxContainer
 # Define a custom signal which sends the username and the assigned task name.
 
-signal task_assigned(username: String, task_name: String)
+#signal task_assigned(username: String, task_name: String)
 
 func _on_memory_button_pressed() -> void:
-	var username = $UserName.text
-	print("Assigning Task 1 to user: " + username)
+	var username = int($UserName.text)
+	print("Assigning Task 1 to user: " + str(username))
 	#Add string later
-	Database.add_task(SessionManager.user_number, 1)
+	Database.add_task(username, "memory")
 	
 func _on_focus_button_pressed() -> void:
-	var username = $UserName.text
-	print("Assigning Task 2 to user: " + username)
+	var username = int($UserName.text)
+	print("Assigning Task 2 to user: " + str(username))
 	#Add string later
-	Database.add_task(SessionManager.user_number, 1)
+	Database.add_task(username, "focus")
 	
 func _on_reaction_button_pressed() -> void:
-	var username = $UserName.text
-	print("Assigning Task 3 to user: " + username)
+	var username = int($UserName.text)
+	print("Assigning Task 3 to user: " + str(username))
 	#Add string later
-	Database.add_task(SessionManager.user_number, 1)
+	Database.add_task(username, "reaction")
