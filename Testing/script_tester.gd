37a723cc422b0033d74de9e098d0d96c5ba67d08
Lee-Roy <21064014+Lee-RoyDobson@users.noreply.github.com>
2025-04-25 15:16:28 +0100

    Fixed script_tester

--- Testing/script_tester.gd
+++ Testing/script_tester.gd
@@ -5,7 +5,7 @@ func _on_save_record_pressed() -> void:
 	Database.save_record(123456, "GO HOME!", 50)
 
 func _on_add_employee_pressed() -> void:
-	Database.add_employee(100597, "password123", 0)
+	Database.add_employee(100597, "password123")
 
 func _on_remove_task_pressed() -> void:
 	Database.remove_task(100597, "focus")
